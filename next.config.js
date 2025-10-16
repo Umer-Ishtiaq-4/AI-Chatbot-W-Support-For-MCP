620c033f633c3e6657db519901fe48d08db7b8fd
Umer Ishtiaq <umerishtiaq4@gmail.com>
2025-10-16 15:28:51 +0500

    Docker file updated #7

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	webpack: (config) => {
+		config.resolve = config.resolve || {}
+		config.resolve.alias = {
+			...(config.resolve.alias || {}),
+			'@': path.resolve(__dirname),
+		}
+		return config
+	},
+}
 
 module.exports = nextConfig
 
